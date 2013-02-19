bf7211c1005f26dae3e2d170e088eee7affe768c
AngleWyrm <anglewyrm@yahoo.com>
2013-02-19 11:58:22 -0800

    Close button and 'e' functionality

--- GuiAnalyzer.java
+++ GuiAnalyzer.java
@@ -27,6 +27,29 @@ public class GuiAnalyzer extends GuiScreen {
 		int posY = (this.height - ySizeOfTexture) / 2;
 		
 		this.controlList.add(new GuiButton(0, posX+xSizeOfTexture - 45, posY + 4, 40, 20, "Scan"));
+		this.controlList.add(new GuiButton(1, posX+xSizeOfTexture - 45, posY + 28, 40, 20, "Close"));
+	}
+	
+	public void actionPerformed(GuiButton button)
+	{
+		switch(button.id){
+		case 0: // Scan button
+			break;
+		case 1: // Close button
+		default:
+			this.mc.thePlayer.closeScreen(); 
+		}
+	}
+	@Override
+	public boolean doesGuiPauseGame(){
+		return false;
+	}
+	
+	@Override
+	protected void keyTyped(char par1, int par2){
+		if (par2 == 1 || par2 == this.mc.gameSettings.keyBindInventory.keyCode){
+			this.mc.thePlayer.closeScreen();
+		}
 	}
 	
 	@Override
